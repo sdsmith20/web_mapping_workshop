7ff7d5522e38fe26d4a63036cb0761a46327c54c
sdsmith20 <smith-steve@mssu.edu>
2016-04-25 09:36:59 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -13,6 +13,22 @@ L.mapbox.accessToken = accessToken;
 var map = L.mapbox.map('map', mapId);
 
 // Set the initial view of the map to the whole US
-map.setView([-39, 180], 7);
+map.setView([39, -96], 4);
 
 // Great, now we have a basic web map!
+
+var dataFileToAdd = 'data/parks.geojson';
+
+var featureLayer = L.mapbox.feaureLayer()
+feaureLayer.loadURL(dataFileToAdd);
+featuerLayer.addTo(map);
+
+featureLayer.on('ready', function(){
+  this.setStyle({
+  "color":"#6583BF",
+  "fillCOlor": "#6583BF",
+  "weight": .5,
+  "opacity"0.65
+  })
+  map.fitBounds(featureLayer.getBounds());
+})
