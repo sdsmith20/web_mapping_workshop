d68648816651f16eb225c66fb80ab1168530455d
sdsmith20 <smith-steve@mssu.edu>
2016-04-25 10:27:55 -0500

    Updated test1.js

--- test1.js
+++ test1.js
@@ -17,7 +17,7 @@ map.setView([39, -96], 4);
 
 // Great, now we have a basic web map!
 
-var dataFileToAdd = 'data/powercat.geojson';
+var dataFileToAdd = 'data/resturants.geojson';
 
 var featureLayer = L.mapbox.featureLayer();
 featureLayer.loadURL(dataFileToAdd);
