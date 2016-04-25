9d619a83312072a00760e0082f382b9fe7d01d62
sdsmith20 <smith-steve@mssu.edu>
2016-04-25 11:02:07 -0500

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -56,7 +56,7 @@ var clickHandler = function(e){
 
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    		layer.on('clcik',clickHandler);
+    		layer.on('click',clickHandler);
                                 })
 })
 
