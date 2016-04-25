516b8bb237059b78131b664f48d05b415393776d
sdsmith20 <smith-steve@mssu.edu>
2016-04-25 10:22:09 -0500

    Updated test1.js

---
+++ test1.js
@@ -0,0 +1,40 @@
+// Here is the javascript setup for a basic map:
+
+// Enter your mapbox map id here to reference it for the base layer,
+// this one references the ugly green map that I made.
+var mapId = 'sdsmith20.pp9k1202';
+
+// And this is my access token, use yours.
+var accessToken = 'pk.eyJ1Ijoic2RzbWl0aDIwIiwiYSI6ImNpbmcwZmd6MjE5bzV1OGx3bmpkbTMzM2QifQ.hSn3ilMmtqR5Nhd_ssFrww';
+
+// Create the map object with your mapId and token, 
+// referencing the DOM element where you want the map to go.
+L.mapbox.accessToken = accessToken;
+var map = L.mapbox.map('map', mapId);
+
+// Set the initial view of the map to the whole US
+map.setView([39, -96], 4);
+
+// Great, now we have a basic web map!
+
+var dataFileToAdd = 'data/resturants.geojson';
+
+var featureLayer = L.mapbox.featureLayer();
+featureLayer.loadURL(dataFileToAdd);
+featureLayer.addTo(map);
+
+featureLayer.on('ready', function(){
+  this.setStyle({
+  "color":"#6583BF",
+  "fillColor": "#6583BF",
+  "weight": .5,
+  "opacity": 0.65
+  })
+  map.fitBounds(featureLayer.getBounds());
+})
+
+featureLayer.on('ready',function(){
+  this.eachLayer(function(layer){
+                 layer.bindPopup('Welcome to ' +
+                                 layer.feature.properties.amenity);})
+})
