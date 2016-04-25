e10d27276dc16e34080282ca943d3ac014a98afd
sdsmith20 <smith-steve@mssu.edu>
2016-04-25 10:24:36 -0500

    Updated test1.js

--- test1.js
+++ test1.js
@@ -33,7 +33,7 @@ featureLayer.on('ready', function(){
   map.fitBounds(featureLayer.getBounds());
 })
 
-featureLayer.on('ready',function(){
+//featureLayer.on('ready',function(){
   this.eachLayer(function(layer){
                  layer.bindPopup('Welcome to ' +
                                  layer.feature.properties.amenity);})
