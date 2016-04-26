4613bdb2836f6384282201c0bab489bd24b1a624
sdsmith20 <smith-steve@mssu.edu>
2016-04-26 10:08:25 -0500

    Update test1.js

--- test1.js
+++ test1.js
@@ -2,7 +2,7 @@
 
 // Enter your mapbox map id here to reference it for the base layer,
 // this one references the ugly green map that I made.
-var mapId = 'sdsmith20.pp9k1202';
+var mapId = 'sdsmith20.pp97edd3';
 
 // And this is my access token, use yours.
 var accessToken = 'pk.eyJ1Ijoic2RzbWl0aDIwIiwiYSI6ImNpbmcwZmd6MjE5bzV1OGx3bmpkbTMzM2QifQ.hSn3ilMmtqR5Nhd_ssFrww';
@@ -13,7 +13,7 @@ L.mapbox.accessToken = accessToken;
 var map = L.mapbox.map('map', mapId);
 
 // Set the initial view of the map to the whole US
-map.setView([39, -96], 4);
+map.setView([37.157348, -94.534003], 4);
 
 // Great, now we have a basic web map!
 
